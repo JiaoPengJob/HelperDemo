a9a3f141028e2bd6c8e5c4b4d13dbc978058449c
JiaoPeng <jiaopengjob@outlook.com>
2016-06-02 22:18:49 +0800

    Screen
    Screen

---
+++ HelperDemo/app/src/main/java/com/helper/jiaop/helpers/ScreenUtils.java
@@ -0,0 +1,33 @@
+package com.helper.jiaop.helpers;
+
+import android.content.Context;
+
+/**
+ * Created by jiaop on 2016/6/2.
+ * 屏幕单位转换
+ */
+public class ScreenUtils {
+
+    public static float dpToPx(Context context, float dp) {
+        if (context == null) {
+            return -1;
+        }
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float pxToDp(Context context, float px) {
+        if (context == null) {
+            return -1;
+        }
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static int dpToPxInt(Context context, float dp) {
+        return (int)(dpToPx(context, dp) + 0.5f);
+    }
+
+    public static int pxToDpCeilInt(Context context, float px) {
+        return (int)(pxToDp(context, px) + 0.5f);
+    }
+
+}
