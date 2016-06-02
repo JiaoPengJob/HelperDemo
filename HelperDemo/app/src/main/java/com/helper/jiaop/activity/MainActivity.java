bdd799fa744dad0ed7aab9b7e74c4b06635ae54d
JiaoPeng <jiaopengjob@outlook.com>
2016-06-02 19:54:41 +0800

    helper
    helper

---
+++ HelperDemo/app/src/main/java/com/helper/jiaop/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.helper.jiaop.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.helper.jiaop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
