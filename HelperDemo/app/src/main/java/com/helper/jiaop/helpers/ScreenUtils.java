54bec25d2121daede27cf794524838d2275d5cde
JiaoPeng <jiaopengjob@outlook.com>
2016-06-02 22:23:04 +0800

    change
    change

--- HelperDemo/app/src/main/java/com/helper/jiaop/helpers/ScreenUtils.java
+++ HelperDemo/app/src/main/java/com/helper/jiaop/helpers/ScreenUtils.java
@@ -1,12 +1,38 @@
 package com.helper.jiaop.helpers;
 
+import android.app.Activity;
 import android.content.Context;
 
 /**
  * Created by jiaop on 2016/6/2.
- * 屏幕单位转换
+ * 屏幕或单位转换
  */
 public class ScreenUtils {
+    /**
+     * 获取屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        if (context == null) {
+            return 0;
+        }
+        return ((Activity) context).getWindowManager().getDefaultDisplay().getHeight();
+    }
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        if (context == null) {
+            return 0;
+        }
+        return ((Activity) context).getWindowManager().getDefaultDisplay().getWidth();
+    }
 
     public static float dpToPx(Context context, float dp) {
         if (context == null) {
@@ -23,11 +49,11 @@ public class ScreenUtils {
     }
 
     public static int dpToPxInt(Context context, float dp) {
-        return (int)(dpToPx(context, dp) + 0.5f);
+        return (int) (dpToPx(context, dp) + 0.5f);
     }
 
     public static int pxToDpCeilInt(Context context, float px) {
-        return (int)(pxToDp(context, px) + 0.5f);
+        return (int) (pxToDp(context, px) + 0.5f);
     }
 
 }
