59ea755f7d88ec41891e7698add82ccadc75b9d5
JiaoPeng <jiaopengjob@outlook.com>
2016-06-02 20:49:55 +0800

    stringUtils
    stringUtils

---
+++ HelperDemo/app/src/main/java/com/helper/jiaop/helpers/StringUtils.java
@@ -0,0 +1,131 @@
+package com.helper.jiaop.helpers;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created by jiaop on 2016/6/2.
+ * 常见字符串操作
+ */
+public class StringUtils {
+
+    /**
+     * 判断字符串是否为空或长度为0 或由空格组成
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isBlank(String str) {
+        return (str == null || str.trim().length() == 0);
+    }
+
+    /**
+     * 判断字符串是否为空或长度为0
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(CharSequence str) {
+        return (str == null || str.length() == 0);
+    }
+
+    /**
+     * 获取字符串长度
+     *
+     * @param str
+     * @return
+     */
+    public static int length(CharSequence str) {
+        return str == null ? 0 : str.length();
+    }
+
+    /**
+     * 首字母大写
+     *
+     * @param str
+     * @return
+     */
+    public static String capitalizeFirstLetter(String str) {
+        if (isEmpty(str)) {
+            return str;
+        }
+        char c = str.charAt(0);
+        return (!Character.isLetter(c) || Character.isUpperCase(c)) ? str : new StringBuilder(str.length())
+                .append(Character.toUpperCase(c)).append(str.substring(1)).toString();
+    }
+
+    /**
+     * 以utf-8格式编码
+     *
+     * @param str
+     * @return
+     */
+    public static String utf8Encode(String str) {
+        if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("UnsupportedEncodingException occurred. ", e);
+            }
+        }
+        return str;
+    }
+
+    /**
+     * 处理HTML特殊字符
+     *
+     * @param source
+     * @return
+     */
+    public static String htmlEscapeCharsToString(String source) {
+        return StringUtils.isEmpty(source) ? source : source.replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+                .replaceAll("&amp;", "&").replaceAll("&quot;", "\"");
+    }
+
+    /**
+     * 转换半角字符为全角字符
+     *
+     * @param s
+     * @return
+     */
+    public static String fullWidthToHalfWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 12288) {
+                source[i] = ' ';
+            } else if (source[i] >= 65281 && source[i] <= 65374) {
+                source[i] = (char) (source[i] - 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+
+    /**
+     * 转换全角字符为半角字符
+     *
+     * @param s
+     * @return
+     */
+    public static String halfWidthToFullWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                source[i] = (char) 12288;
+            } else if (source[i] >= 33 && source[i] <= 126) {
+                source[i] = (char) (source[i] + 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+
+}
