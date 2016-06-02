7a6cf54cf7fb631c2133f811a3a8f8b65bd9d2d0
JiaoPeng <jiaopengjob@outlook.com>
2016-06-02 22:47:55 +0800

    soap
    soap

---
+++ SoapDemo/app/src/main/java/com/soap/jiaop/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soap.jiaop.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.soap.jiaop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
