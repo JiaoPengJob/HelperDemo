636a8ad71f6cb9555ff7bab88009dd2869587b52
JiaoPeng <jiaopengjob@outlook.com>
2016-06-06 14:17:49 +0800

    new
    new volley

--- VolleyDemo/app/src/main/java/com/jiaop/volley/activity/MainActivity.java
+++ VolleyDemo/app/src/main/java/com/jiaop/volley/activity/MainActivity.java
@@ -1,15 +1,140 @@
 package com.jiaop.volley.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.jiaop.volley.R;
+import com.jiaop.volley.managers.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void jsonExec() {
+        String tag_json_obj = "json_obj_req";
+        String url = "http://api.androidhive.info/volley/person_object.json";
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response, String str) {
+                        Log.d("TAG", response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error, String str) {
+                pDialog.hide();
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+
+    private void jsonArrayExec() {
+        // Tag used to cancel the request
+        String tag_json_arry = "json_array_req";
+
+        String url = "http://api.androidhive.info/volley/person_array.json";
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response, String str) {
+                        Log.d("TAG", response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error, String str) {
+                pDialog.hide();
+            }
+        });
+        AppController.getInstance().addToRequestQueue(req, tag_json_arry);
+    }
+
+    private void stringExec() {
+        String tag_string_req = "string_req";
+        String url = "http://api.androidhive.info/volley/string_response.html";
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+        StringRequest strReq = new StringRequest(Request.Method.GET,
+                url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response, String str) {
+                Log.d("TAG", response.toString());
+                pDialog.hide();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error, String str) {
+                pDialog.hide();
+            }
+        });
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void jsonByParams() {
+        String tag_json_obj = "json_obj_req";
+        String url = "http://api.androidhive.info/volley/person_object.json";
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response, String str) {
+                        Log.d("TAG", response.toString());
+                        pDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error, String str) {
+                pDialog.hide();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                headers.put("apiKey", "xxxxxxxxxxxxxxx");
+                return headers;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+
 }
