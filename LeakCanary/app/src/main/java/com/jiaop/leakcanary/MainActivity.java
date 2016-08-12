350ce1d9215d7b3bcb13e98baf365febc23abf11
JiaoPeng <jiaopengjob@outlook.com>
2016-08-12 17:22:08 +0800

    change
    many change

--- LeakCanary/app/src/main/java/com/jiaop/leakcanary/MainActivity.java
+++ LeakCanary/app/src/main/java/com/jiaop/leakcanary/MainActivity.java
@@ -3,11 +3,15 @@ package com.jiaop.leakcanary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.squareup.leakcanary.RefWatcher;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RefWatcher watch =  MyApplication.getRefWatcher(this);
+        watch.watch("");
     }
 }
